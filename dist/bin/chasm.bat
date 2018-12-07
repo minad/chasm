c8706ec99a7a4eb37334315a822466a4fe833456
Daniel Mendler <mail@daniel-mendler.de>
2018-12-07 19:03:25 +0100

    keep classpath

--- dist/bin/chasm.bat
+++ dist/bin/chasm.bat
@@ -63,7 +63,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\chasm.jar
+set CLASSPATH=%APP_HOME%\lib\chasm.jar;%CLASSPATH%
 
 @rem Execute chasm
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %CHASM_OPTS%  -classpath "%CLASSPATH%" chasm.Main %CMD_LINE_ARGS%
